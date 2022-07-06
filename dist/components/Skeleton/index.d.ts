6ac59a5989ee9c20e6e9af93667871f33d899f3f
afanti <afanti@juexiaotime.com>
2022-07-06 15:33:48 +0800

    统一参数格式

---
+++ dist/components/Skeleton/index.d.ts
@@ -0,0 +1,49 @@
+declare const _default: import("vue").DefineComponent<{
+    bg: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    width: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    height: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    animated: {
+        type: BooleanConstructor;
+        required: false;
+        default: boolean;
+    };
+}, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    bg: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    width: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    height: {
+        type: StringConstructor;
+        required: false;
+        default: string;
+    };
+    animated: {
+        type: BooleanConstructor;
+        required: false;
+        default: boolean;
+    };
+}>>, {
+    bg: string;
+    width: string;
+    height: string;
+    animated: boolean;
+}>;
+export default _default;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,7 +3,8 @@ import message from "./components/Message/message";
 import Dialog from "./components/Dialog/index.vue";
 import notify from "./components/Notification/notify";
 import Button from "./components/Button/index.vue";
-export { confirm, message, notify, Dialog, Button };
+import Skeleton from "./components/Skeleton/index.vue";
+export { confirm, message, notify, Dialog, Button, Skeleton };
 declare const _default: {
     install: (app: any) => void;
 };
