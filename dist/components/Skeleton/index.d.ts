debfc7e24e10c039311f966de59aa86a6f409df0
afanti <afanti@juexiaotime.com>
2022-07-06 15:07:17 +0800

    添加骨架组件

---
+++ dist/components/Skeleton/index.d.ts
@@ -0,0 +1,48 @@
+declare const _default: import("vue").DefineComponent<{
+    bg: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    width: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    height: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    animated: {
+        type: BooleanConstructor;
+        required: true;
+        default: boolean;
+    };
+}, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, Record<string, any>, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    bg: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    width: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    height: {
+        type: StringConstructor;
+        required: true;
+        default: string;
+    };
+    animated: {
+        type: BooleanConstructor;
+        required: true;
+        default: boolean;
+    };
+}>>, {
+    bg: string;
+    width: string;
+    height: string;
+}>;
+export default _default;
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import message from "./components/Message/message";
 import Dialog from "./components/Dialog/index.vue";
 import notify from "./components/Notification/notify";
 import Button from "./components/Button/index.vue";
+import Skeleton from "./components/Skeleton/index.vue";
 
-const components = [Dialog, Button];
+const components = [Dialog, Button, Skeleton];
 
 // 全局注册
 const install = (app): void => {
@@ -15,7 +16,7 @@ const install = (app): void => {
 };
 
 // 局部注册
-export { confirm, message, notify, Dialog, Button };
+export { confirm, message, notify, Dialog, Button, Skeleton };
 export default {
   install,
 };
